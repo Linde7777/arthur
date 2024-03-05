9b2aaba0d2dd3c94df7e4f063955e2e38bc3e073
LongPeng <longpeng1899@163.com>
2024-03-05 19:40:55 +0800

    feat: 服务器基本功能演示

---
+++ arthurnet/server_test.go
@@ -0,0 +1,51 @@
+package arthurnet
+
+import (
+	"fmt"
+	"net"
+	"testing"
+	"time"
+)
+
+func TestServerBasic(t *testing.T) {
+	s := &Server{
+		Name:      "Arthur",
+		IPVersion: "tcp4",
+		IP:        "0.0.0.0",
+		Port:      1899,
+	}
+	go s.Serve()
+
+	// 等待服务端启动
+	time.Sleep(time.Second * 3)
+	go MockClient()
+	for {
+		// 防止主goroutine退出
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func MockClient() {
+	conn, err := net.Dial("tcp", "0.0.0.0:1899")
+	if err != nil {
+		fmt.Println("client dial err: ", err)
+		return
+	}
+
+	for {
+		_, err = conn.Write([]byte("I am John Marston"))
+		if err != nil {
+			fmt.Println("conn write err: ", err)
+			return
+		}
+
+		buf := make([]byte, 512)
+		count, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("conn read err: ", err)
+			return
+		}
+		fmt.Printf("Receive from server: %s\n", string(buf[:count]))
+	}
+
+}
